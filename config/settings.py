03d98d54a1404ecd3a7e8e1ed3cb29f1895794b9
adityamulik <aditya.mulik@gmail.com>
2021-09-03 06:21:13 -0400

    updated base_dir

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static_media/')
-STATICFILES_DIRS = ( os.path.join(PROJECT_DIR,'static/'),)
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static/'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
